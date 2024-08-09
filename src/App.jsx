4752c5585e2f4996c897df3b027e20681b2e30e8
madyanes <madyanes.dev@gmail.com>
2024-08-09 19:24:55 +0700

    Display converted currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -9,30 +10,74 @@ function App() {
 }
 
 export function Currency() {
+  const [amount, setAmount] = useState(1)
+  const [fromCurrency, setFromCurrency] = useState('USD')
+  const [toCurrency, setToCurrency] = useState('IDR')
+  const [converted, setConverted] = useState(0)
+
+  useEffect(() => {
+    async function converterAPI() {
+      const response = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      )
+      const data = await response.json()
+      setConverted(data.rates[toCurrency])
+    }
+
+    converterAPI()
+  }, [amount, fromCurrency, toCurrency])
+
   return (
     <div className='currency-app noto-serif-font'>
       <h1>Currency Converter</h1>
-      <CurrencyForm />
+      <CurrencyForm
+        amount={amount}
+        fromCurrency={fromCurrency}
+        toCurrency={toCurrency}
+        onToCurrencyChange={(e) => {
+          setToCurrency(e.target.value)
+        }}
+        converted={converted}
+      />
     </div>
   )
 }
 
-export function CurrencyForm() {
+export function CurrencyForm({
+  amount,
+  fromCurrency,
+  toCurrency,
+  onToCurrencyChange,
+  converted,
+}) {
   return (
-    <form className='currency-form' onSubmit={(e) => e.preventDefault()}>
-      <CurrencyInput />
-      <CurrencyInput />
-    </form>
+    <div>
+      <form className='currency-form' onSubmit={(e) => e.preventDefault()}>
+        <CurrencyInput />
+        <CurrencyInput
+          disabled
+          converted={converted}
+          toCurrency={toCurrency}
+          onToCurrencyChange={onToCurrencyChange}
+        />
+      </form>
+    </div>
   )
 }
 
-export function CurrencyInput() {
+export function CurrencyInput({
+  disabled,
+  converted,
+  toCurrency,
+  onToCurrencyChange,
+}) {
   return (
     <div className='currency-input'>
-      <input type='text' />
-      <select name='' id=''>
+      <input type='text' value={converted} disabled={disabled} />
+      <select name='' id='' value={toCurrency} onChange={onToCurrencyChange}>
         <option value='IDR'>IDR</option>
         <option value='USD'>USD</option>
+        <option value='JPY'>JPY</option>
       </select>
     </div>
   )
