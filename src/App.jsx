82e7beb4903caaa8f1611f66958aa8d955646f9d
madyanes <madyanes.dev@gmail.com>
2024-08-09 14:04:17 +0700

    Layout design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import './App.css'
+
 function App() {
   return (
     <>
-      <h1>Currency Converter</h1>
+      <Currency />
     </>
+  )
+}
+
+export function Currency() {
+  return (
+    <div className='currency-app noto-serif-font'>
+      <h1>Currency Converter</h1>
+      <CurrencyForm />
+    </div>
+  )
+}
+
+export function CurrencyForm() {
+  return (
+    <form className='currency-form' onSubmit={(e) => e.preventDefault()}>
+      <CurrencyInput />
+      <CurrencyInput />
+    </form>
+  )
+}
+
+export function CurrencyInput() {
+  return (
+    <div className='currency-input'>
+      <input type='text' />
+      <select name='' id=''>
+        <option value='IDR'>IDR</option>
+        <option value='USD'>USD</option>
+      </select>
+    </div>
   )
 }
 
